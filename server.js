d2e4959daee903eb437efc727b8de7271238f1c3
kvfloresr <karenvfloresr@gmail.com>
2024-04-14 22:47:56 -0400

    ready

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, 'public', 'pages','sign-in.html'));
+});
+
+
+app.listen(PORT, () => {
+console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
